0dc5e5e0df47c83bfb2f28bd43311cbb3ffc7bbd
ByeongHyeon <qudgus1984@naver.com>
2022-03-22 17:23:54 +0900

    IBOutlet 완료

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
